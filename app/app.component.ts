f558c86eb1b803017bbc94c86fc68b0d22090588
nayami <cibulnicoleg@gmail.com>
2016-09-06 20:56:38 +0300

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+	selector: 'app-component',
+	template: `
+		Lorem ipsum dolor sit amet.
+	`,
+})
+
+export class AppComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+}
